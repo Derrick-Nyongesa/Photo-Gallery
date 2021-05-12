d9e75c03792a561ca4b38d84c3716ee8ff8fdefe
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 14:48:58 +0300

    added methods to the Image model

--- photo/models.py
+++ photo/models.py
@@ -26,3 +26,32 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls, category):
+        images = cls.objects.filter(category__name__icontains=category)
+        return images
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
