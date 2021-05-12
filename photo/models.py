e25df70f5c06f0d757d4eb2917d8b1f3219b8a47
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 14:51:19 +0300

    implemented get, update, save and delete location to the location model

--- photo/models.py
+++ photo/models.py
@@ -7,7 +7,23 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
-    
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
 
 class Category(models.Model):
