c224629151db9e6271383e81ca7fd4d6520c8661
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 13:07:27 +0300

    Created Image model

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to = 'images/',default='DEFAULT VALUE')
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
