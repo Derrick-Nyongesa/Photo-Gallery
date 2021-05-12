453ab64990e6aa7f711a77893491755fa9ec9ede
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 13:10:39 +0300

    Created Location model

--- photo/models.py
+++ photo/models.py
@@ -10,3 +10,10 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
