5477d76272879ccb53e4902b314bddd54a39faf1
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 18:02:43 +0300

    uploaded images to the application

--- photo/views.py
+++ photo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Image,Location
 
 # Create your views here.
 def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
 
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'images': images[::-1], 'locations': locations})
