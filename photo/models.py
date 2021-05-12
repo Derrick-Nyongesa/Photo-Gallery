183de2b21d7911079cccc615f94fad408d870bf1
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 13:12:40 +0300

    created Category model

--- photo/models.py
+++ photo/models.py
@@ -17,3 +17,10 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
