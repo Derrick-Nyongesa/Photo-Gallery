08bc980853128d25a1cdb866f90173c8da31069c
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 14:50:35 +0300

    Implemented save and delete methods to the Category model

--- photo/models.py
+++ photo/models.py
@@ -7,12 +7,20 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    
+
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
 
 class Image(models.Model):
