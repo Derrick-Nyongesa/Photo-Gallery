254a8d4182a820901c2421a103616ae671d2ddc7
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-12 14:22:53 +0300

    created database relationships

--- photo/models.py
+++ photo/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    name = models.CharField(max_length=60)
-    image = models.ImageField(upload_to = 'images/',default='DEFAULT VALUE')
-    description = models.TextField()
-    author = models.CharField(max_length=40, default='admin')
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Location(models.Model):
     name = models.CharField(max_length=60)
 
@@ -21,6 +10,19 @@ class Location(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to = 'images/',default='DEFAULT VALUE')
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default='')
+    location = models.ForeignKey(Location,on_delete=models.CASCADE, default='')
 
     def __str__(self):
         return self.name
