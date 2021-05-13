b250e105768aaba08d34001860e3ca623b7ef8c9
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-05-13 16:35:33 +0300

    added titles to the pages

--- photo/views.py
+++ photo/views.py
@@ -6,8 +6,9 @@ def index(request):
     images = Image.objects.all()
     locations = Location.get_locations()
     print(locations)
+    title = "Home"
 
-    return render(request, 'index.html',{'images': images[::-1], 'locations': locations})
+    return render(request, 'index.html',{'images': images[::-1], 'locations': locations, "title":title})
 
 
 def search(request):
@@ -16,14 +17,17 @@ def search(request):
         searched_images = Image.search_by_category(category)
         message = f"{category}"
         print(searched_images)
-        return render(request, 'search.html', {"message": message, "images": searched_images})
+        title = "Searched Images"
+        return render(request, 'search.html', {"message": message, "images": searched_images, "title":title})
     else:
         message = "You haven't searched for any image category"
+        title = "Searched Images"
 
-        return render(request, 'search.html', {"message": message})
+        return render(request, 'search.html', {"message": message, "title":title})
 
 
 def location(request, location):
     images = Image.filter_by_location(location)
     print(images)
-    return render(request, 'location.html', {'location_images': images})
+    title = "Location"
+    return render(request, 'location.html', {'location_images': images, "title":title})
